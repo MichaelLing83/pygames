1e76cbe701bc6c0579635803f9b1bd874cb01b07
Michael Ling <michael.ling.se@gmail.com>
2022-08-15 18:22:18 -0400

    env.sh

--- env.sh
+++ env.sh
@@ -3,6 +3,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-virtualenv -p python3 .venv
-source .venv/bin/activate
-pip3 install -r requirements.txt
+pipenv install
